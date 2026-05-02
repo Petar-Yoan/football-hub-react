4f07afd55599cc0d84ba91613359ec0702e42e5c
Petar-Yoan Tsvetanov <petaryoantsvetanov@gmail.com>
2026-05-02 22:11:30 +0300

    Set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css';
+import { Route, Routes } from 'react-router';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
-
+import Teams from './pages/Teams';
+import Matches from './pages/Matches';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/matches" element={<Matches />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   );
 }
+
 export default App;
