989db30f414c2a33dad44bff09c3c2ce9b001a74
Petar-Yoan Tsvetanov <petaryoantsvetanov@gmail.com>
2026-05-02 22:37:49 +0300

    Improve navbar style

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,10 +2,16 @@ import { Link } from 'react-router';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/teams">Teams</Link> |{' '}
-      <Link to="/matches">Matches</Link> | <Link to="/login">Login</Link> |{' '}
-      <Link to="/register">Register</Link>
+    <nav className="navbar">
+      <div className="navbar__brand">Football World</div>
+
+      <div className="navbar__links">
+        <Link to="/">Home</Link>
+        <Link to="/teams">Teams</Link>
+        <Link to="/matches">Matches</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
     </nav>
   );
 }
